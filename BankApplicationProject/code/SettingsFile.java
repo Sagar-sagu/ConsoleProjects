7a71dfe2a37d13af46a59ae65724e0fa839d551d
Sagar-sagu <sagargk307@gmail.com>
2025-10-03 23:35:20 +0530

    Secondt commit

--- BankApplicationProject/code/SettingsFile.java
+++ BankApplicationProject/code/SettingsFile.java
@@ -4,6 +4,8 @@ public class SettingsFile
 {
 	public static void settings
 	{
+		System.out.println("");
+		System.out.println("Here you can Set Your MPIN PIN");
 		
 	}
 }
---
+++ BankApplicationProject/code/viewUsrBankReletedDetailes.java
@@ -0,0 +1,14 @@
+package code;
+
+public class viewUsrBankReletedDetailes
+{
+	public static void viewPassBookInterFace
+	{
+		
+	}
+
+	public static void viewLastTransactionInPassbook
+	{
+		
+	}
+}
